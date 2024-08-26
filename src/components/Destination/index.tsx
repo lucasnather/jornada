860727c8b57c9f273652f851b512f85a0a12586e
lucasnather <natherlucas@gmail.com>
2024-08-26 16:01:48 -0400

    feat: implement destinations section

--- src/components/Destination/index.tsx
+++ src/components/Destination/index.tsx
@@ -1,5 +1,6 @@
-import { ButtonFilter, ContainerForm, ContainerInput, ContainerMain, InputFilter } from "./styled";
+import { ButtonDetails, ButtonFilter, City, ContainerDestination, ContainerDestinationCard, ContainerDestinationContent, ContainerForm, ContainerInput, ContainerMain, InputFilter, Price, Title } from "./styled";
 import Deserto from '../../assets/deserto.png'
+import Thumb from '../../assets/thumbnail.png'
 
 export function Destination() {
     return(
@@ -14,27 +15,63 @@ export function Destination() {
             </ContainerInput>
 
             <section>
-                <h2>Destino</h2>
+                <Title>Destinos</Title>
+
+                <ContainerDestination>
+                    <ContainerDestinationCard>
+                        <img src={Deserto} alt="" />
+                        <ContainerDestinationContent>
+                            <City>Atacama</City>
+                            <Price>$ 500</Price>
+                            <ButtonDetails>Ver detalhes</ButtonDetails>
+                        </ContainerDestinationContent>
+                    </ContainerDestinationCard>
+
+                    <ContainerDestinationCard>
+                        <img src={Deserto} alt="" />
+                        <ContainerDestinationContent>
+                            <City>Atacama</City>
+                            <Price>$ 500</Price>
+                            <ButtonDetails>Ver detalhes</ButtonDetails>
+                        </ContainerDestinationContent>
+                    </ContainerDestinationCard>
+
+                    <ContainerDestinationCard>
+                        <img src={Deserto} alt="" />
+                        <ContainerDestinationContent>
+                            <City>Atacama</City>
+                            <Price>$ 500</Price>
+                            <ButtonDetails>Ver detalhes</ButtonDetails>
+                        </ContainerDestinationContent>
+                    </ContainerDestinationCard>
+                </ContainerDestination>
+            </section>
+
+            <section>
+                <Title>Depoimentos</Title>
 
                 <div>
-                    <img src={Deserto} alt="" />
-                    <p>Atacama</p>
-                    <p>$ 500</p>
-                    <button>Ver detalhes</button>
-                </div>
-
-                <div>
-                    <img src={Deserto} alt="" />
-                    <p>Atacama</p>
-                    <p>$ 500</p>
-                    <button>Ver detalhes</button>
-                </div>
-
-                <div>
-                    <img src={Deserto} alt="" />
-                    <p>Atacama</p>
-                    <p>$ 500</p>
-                    <button>Ver detalhes</button>
+                    <div>
+                        <img src={Thumb} alt="" />
+                        <div>
+                            <p>A Jornada foi uma das melhores agências de viagens que eu já experimentei. O serviço ao cliente foi excepcional, e toda a equipe foi muito atenciosa e prestativa.</p>
+                            <p>Lauro Matos</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={Thumb} alt="" />
+                        <div>
+                            <p>Recomendo fortemente a agência de viagens Jornada. Eles oferecem um serviço personalizado e de alta qualidade que excedeu minhas expectativas em minha última viagem.</p>
+                            <p>Lauro Matos</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={Thumb} alt="" />
+                        <div>
+                            <p>Minha viagem com a Jornada foi incrível! Recomendo muito a agência para quem busca uma experiência emocionante e personalizada a partir das nossas necessidades.</p>
+                            <p>Lauro Matos</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </ContainerMain>
--- src/components/Destination/styled.ts
+++ src/components/Destination/styled.ts
@@ -26,14 +26,14 @@ export const ContainerForm = styled.form`
 export const InputFilter = styled.input`
     width: 100%;
     padding: 14px;
-    border-radius: 8px;
+    border-radius: 5px;
 `
 
 export const ButtonFilter = styled.button`
     background-color: ${({theme}) => theme.colors["primary-2"]};
     color: ${({theme}) => theme.colors.white};
     padding: 8px 24px;
-    border-radius: 8px;
+    border-radius: 5px;
     border: none;
 
     &:hover {
@@ -41,3 +41,55 @@ export const ButtonFilter = styled.button`
     }
 
 `
+
+export const Title = styled.h2`
+    font-size: 32px;
+    margin-bottom: 32px;
+    line-height: 40px;
+    font-weight: 400;
+`
+
+export const ContainerDestination = styled.section`
+    display: grid;
+    grid-template-columns: repeat(3, 317.33px);
+    gap: 24px;
+    
+`
+
+export const ContainerDestinationCard = styled.div`
+    background-color: ${({theme}) => theme.colors["primary-4"]};
+    margin-bottom: 32px;
+`
+
+export const ContainerDestinationContent = styled.div`
+    text-align: center;
+    padding: 16px;
+`
+
+export const City = styled.p`
+    font-size: 32px;
+    line-height: 36px;
+`
+
+export const Price = styled.p`
+    font-size: 32px;
+    line-height: 40px;
+    margin: 24px;
+    font-weight: 300;
+`
+
+export const ButtonDetails = styled.button`
+    width: 100%;
+    background-color: ${({theme}) => theme.colors["primary-2"]};
+    color: ${({theme}) => theme.colors.white};
+    padding: 8px 24px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${({theme}) => theme.colors["primary-3"]};
+    }
+`
+
+
